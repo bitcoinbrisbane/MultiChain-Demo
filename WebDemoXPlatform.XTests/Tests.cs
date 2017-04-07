6b0e5deacaf5d8638b92238f6bc6d7cc9c2c7e10
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2017-04-07 22:31:10 +1000

    Add deserialization

--- WebDemoXPlatform.XTests/Tests.cs
+++ WebDemoXPlatform.XTests/Tests.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace WebDemoXPlatform.XTests
@@ -10,7 +6,7 @@ namespace WebDemoXPlatform.XTests
     public class Tests
     {
         [Fact]
-        public void Should_Ser_DTO()
+        public void Should_Serialize_DTO()
         {
             Models.DTOs.Instrument instrument = new Models.DTOs.Instrument()
             {
@@ -20,10 +16,28 @@ namespace WebDemoXPlatform.XTests
                 Name = "UBS"
             };
 
-            String actual = WebDemoXPlatform.Helpers.SerialisationHelper.ToHex(instrument);
+            String actual = Helpers.SerialisationHelper.ToHex(instrument);
             const string expected = "7B224249434944223A2231222C224E616D65223A22554253222C22436F756E747279223A224155222C224D61726B6574223A22457175697479227D";
 
             Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Should_DeSerialize_DTO()
+        {
+            Models.DTOs.Instrument expected = new Models.DTOs.Instrument()
+            {
+                BICID = "1",
+                Country = "AU",
+                Market = "Equity",
+                Name = "UBS"
+            };
+
+            Models.DTOs.Instrument actual = Helpers.SerialisationHelper.ToObject<Models.DTOs.Instrument>("7B224249434944223A2231222C224E616D65223A22554253222C22436F756E747279223A224155222C224D61726B6574223A22457175697479227D");
+            Assert.Equal(expected.BICID, actual.BICID);
+            Assert.Equal(expected.Country, actual.Country);
+            Assert.Equal(expected.Market, actual.Market);
+            Assert.Equal(expected.Name, actual.Name);
         }
     }
 }
--- WebDemoXPlatform/Helpers/SerialisationHelper.cs
+++ WebDemoXPlatform/Helpers/SerialisationHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace WebDemoXPlatform.Helpers
 {
@@ -12,6 +13,17 @@ namespace WebDemoXPlatform.Helpers
 
             String hex = BitConverter.ToString(bytes);
             return hex.Replace("-", "");
+        }
+
+        public static T ToObject<T>(String value)
+        {
+            Byte[] bytes = Enumerable.Range(0, value.Length)
+                     .Where(x => x % 2 == 0)
+                     .Select(x => Convert.ToByte(value.Substring(x, 2), 16))
+                     .ToArray();
+
+            T dto = Newtonsoft.Json.JsonConvert.DeserializeObject<T>(System.Text.Encoding.ASCII.GetString(bytes));
+            return dto;
         }
     }
 }
