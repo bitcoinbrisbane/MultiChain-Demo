954cf9c4f0fdd3860ec1b3d1a29dfcc7fe68d4a2
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2017-05-17 20:18:29 +1000

    Remove old stream item controller.

---
+++ WebDemoXPlatform/ViewModels/StreamItemsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebDemoXPlatform.ViewModels
+{
+    public class StreamItemsViewModel : List<Models.ListStreamsItems.Result>
+    {
+        public String Chain { get; set; }
+
+        public String Stream { get; set; }
+    }
+}
